f9b3ce84ad9e9309480d9c8d286174e25e72973e
Cristian Carlesso <kentaromiura@MacBook-di-Cristian.local>
2012-07-06 18:15:43 +0200

    IOC for mootools
    added every bits it needed to works.

---
+++ README.md
@@ -0,0 +1,3 @@
+this IOC plugin is just a rewrite of this old vanilla js IOC container:
+http://mykenta.blogspot.it/2007/09/kentaioc-js-ioc-library.html,
+to understand what it does, and how it is supposed to work just look at the Spec.
---
+++ Source/kenta.IOC.js
@@ -0,0 +1,58 @@
+/*
+ ---
+ name: kenta.IOC
+ description: IOC for mootools
+ authors: Cristian Carlesso http://mykenta.blogspot.com
+ copyright: Cristian Carlesso
+ license: MIT-style license.
+ requires: []
+ provides: [IOC]
+ ...
+ */
+!function(global){
+	var slice = Array.prototype.slice;
+	var containers = {};
+	global.IOC = new Class({
+
+		initialize:    function(){
+			this.id = String.uniqueID();
+			containers[this.id] = [];
+		},
+		find:          function(id){
+			return containers[this.id][id];
+		},
+		register:      function(id, klass){
+			containers[this.id][id] = klass;
+		},
+		byConstructor: function(id, params){
+			var args = slice.call(arguments);
+			var klass = this.find(args.shift());
+
+			var initialize = klass.prototype.initialize;
+			delete klass.prototype.initialize;
+
+			var instance = new klass;
+
+			if (initialize) {
+				klass.prototype.initialize = initialize;
+				initialize.apply(instance, args);
+			}
+			return instance;
+		},
+		bySetter:      function(id, setter){
+			var args = slice.call(arguments);
+			var klass = this.find(args.shift());
+
+			var initialize = klass.prototype.initialize;
+			delete klass.prototype.initialize;
+
+			var instance = new klass;
+
+			if (initialize) {
+				klass.prototype.initialize = initialize;
+			}
+
+			return Object.merge(instance, setter);
+		}
+	});
+}(this)
